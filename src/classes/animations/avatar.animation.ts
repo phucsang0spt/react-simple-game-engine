59bf37f8c1df96afd2d62fb3c7bab2f158631e5a
phuc sang <phucsang0spt@gmail.com>
2022-05-16 00:23:21 +0700

    Improve AvatarAnimationSprite

--- src/classes/animations/avatar.animation.ts
+++ src/classes/animations/avatar.animation.ts
@@ -1,41 +1,33 @@
 import { AvatarSprite } from "../sprites/avatar.sprite";
 import { AnimationSprite } from "./animation";
 
-export class AvatarAnimationSprite extends AnimationSprite<AvatarSprite> {
-  private options!: {
-    offsetX: number;
-    offsetY: number;
-    offsetWidth: number;
-    offsetHeight: number;
-  };
+type Offset = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
-  initial({
-    offsetX = 0,
-    offsetY = 0,
-    ...options
-  }: {
-    offsetX?: number;
-    offsetY?: number;
-    offsetWidth: number;
-    offsetHeight: number;
-  }) {
-    this.options = {
-      offsetX,
-      offsetY,
-      ...options,
+export class AvatarAnimationSprite extends AnimationSprite<AvatarSprite> {
+  private offset!: Offset;
+
+  initial({ x = 0, y = 0, width = 0, height = 0 }: Partial<Offset>) {
+    this.offset = {
+      x,
+      y,
+      width,
+      height,
     };
   }
 
   protected checkFrameMax(): boolean {
-    const { offsetWidth, offsetX } = this.options;
-    return (
-      this.currentFrame * offsetWidth + offsetX >= this.source.sprite!.width
-    );
+    const { width, x } = this.offset;
+    return this.currentFrame * width + x >= this.source.sprite!.width;
   }
 
   onDraw() {
     const source = this.source;
-    const { offsetWidth, offsetHeight, offsetX, offsetY } = this.options;
+    const { x, y, width, height } = this.offset;
     Renderer.image(
       source.sprite!,
       // position on canvas
@@ -44,10 +36,10 @@ export class AvatarAnimationSprite extends AnimationSprite<AvatarSprite> {
       source.width,
       source.height,
       //crop on source image
-      this.currentFrame * offsetWidth + offsetX,
-      offsetY,
-      offsetWidth,
-      offsetHeight
+      this.currentFrame * width + x,
+      y,
+      width,
+      height
     );
   }
 }
